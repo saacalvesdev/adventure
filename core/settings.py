c7ca7cc2ed9318cc5ec35353b57d2b7427b0ab38
saacalvesdev <saacalves.dev@gmail.com>
2023-02-23 21:55:34 -0300

    Update Adventure Models

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'livereload',
     'django.contrib.staticfiles',
     'home',
+    'plans',
     
 ]
 
@@ -79,8 +80,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : 'adventure',
+        'USER' :   'user_con',
+        'PASSWORD' : '13789',
+        'HOST' : 'localhost',
     }
 }
 
---
+++ plans/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def plans(request):
+    return render (request, 'plans/index.html')
